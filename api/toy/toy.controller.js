cf21cf01e9768b20732605805a59b2a3b50707d7
Denis Mokin <denmokin@gmail.com>
2026-05-26 16:49:55 +0300

    advanced backend init

---
+++ api/toy/toy.controller.js
@@ -0,0 +1,96 @@
+import { toyService } from './toy.service.js'
+import { logger } from '../../services/logger.service.js'
+
+export async function getToys(req, res) {
+    try {
+        const toys = await toyService.query(queryOptions)
+        res.send(toys)
+    }
+    catch (err) {
+        logger.error('Had issues getting toys', err)
+        res.status(500).send({ err: 'Had issues getting toys' })
+    }
+}
+
+export async function getToyById(req, res) {
+    const { id } = req.params
+    try {
+        const toy = await toyService.getById(id)
+        res.send(toy)
+    }
+    catch (err) {
+        logger.error(`Had issues getting toy:${id}`, err)
+        res.status(500).send({ err: `Had issues getting toy:${id}` })
+    }
+}
+
+export async function removeToy(req, res) {
+    const { id } = req.params
+    const loggedInUser = req.verifiedUser
+    try {
+        await toyService.remove(id, loggedInUser)
+        res.send({ msg: 'Toy removed', toyId: id })
+    }
+    catch (err) {
+        logger.error(`Had issues removing toy:${id}`, err)
+        res.status(500).send({ msg: `Had issues removing toy:${id}` })
+    }
+}
+
+export async function addToy(req, res) {
+    const toy = req.body
+    const { loggedinUser } = req
+
+    try {
+        const toyToSave = await toyService.save(toy, loggedInUser)
+        res.send(toyToSave)
+    }
+    catch (err) {
+        logger.error(`Had issues adding toy`, err)
+        res.status(400).send({ msg: 'Had issues adding toy' })
+    }
+}
+
+export async function updateToy(req, res) {
+    const toy = req.body
+    const { loggedinUser } = req
+
+    try {
+        const toyToUpdate = await toyService.save(toy, loggedInUser)
+        res.send(toyToUpdate)
+    }
+    catch (err) {
+        logger.error(`Had issues updating toy`, err)
+        res.status(400).send({ msg: 'Had issues updating toy' })
+    }
+}
+
+export async function addToyMsg(req, res) {
+    const { loggedinUser } = req
+    try {
+        const toyId = req.params.id
+        const msg = {
+            txt: req.body.txt,
+            by: loggedinUser,
+            createdAt: Date.now(),
+        }
+        const savedMsg = await toyService.addToyMsg(toyId, msg)
+        res.json(savedMsg)
+    } catch (err) {
+        logger.error('Failed to add toy massage', err)
+        res.status(500).send({ err: 'Failed to add toy massage' })
+    }
+}
+
+export async function removeToyMsg(req, res) {
+    const { loggedinUser } = req
+    try {
+        const { id: toyId, msgId } = req.params
+
+        const removedId = await toyService.removeToyMsg(toyId, msgId)
+        res.send(removedId)
+    } catch (err) {
+        logger.error('Failed to remove toy msg', err)
+        res.status(500).send({ err: 'Failed to remove toy msg' })
+    }
+}
---
+++ api/toy/toy.service.js
@@ -0,0 +1,145 @@
+import { ObjectId } from 'mongodb'
+
+import { dbService } from '../../services/db.service.js'
+import { logger } from '../../services/logger.service.js'
+import { makeId } from '../../services/util.service.js'
+
+export const Service = {
+	query,
+	getById,
+	remove,
+	add,
+	update,
+	addMsg,
+	removeMsg,
+}
+
+async function query({ filterBy = {} } = {}) {
+	try {
+
+		const collection = await dbService.getCollection('toyStore')
+
+		const criteria = _buildCriteria(filterBy)
+
+		const totalToys = collection.countDocument(criteria)
+
+		const cursor = collection.find(criteria)
+
+		let totalPages = 1
+
+		if (filterBy.pageIdx !== undefined && filterBy.pageSize !== undefined) {
+			const pageIdx = +filterBy.pageIdx
+			const pageSize = +filterBy.pageSize
+
+			cursor = cursor.skip(pageIdx * pageSize).limit(pageSize)
+			totalPages = Math.ceil(totalToys / pageSize) || 1
+		}
+
+		const toys = await cursor.toArray()
+		return { toys, totalPages }
+
+	} catch (err) {
+		logger.error('toyService.query failed:', err)
+		throw err
+	}
+}
+
+async function getById(toyId) {
+	try {
+		const collection = await dbService.getCollection('toyStore')
+		const toy = await collection.findOne({ _id: ObjectId.createFromHexString(toyId) })
+		toy.createdAt = toy._id.getTimestamp()
+		return toy
+	} catch (err) {
+		logger.error(`while finding toy ${toyId}`, err)
+		throw err
+	}
+}
+
+async function remove(toyId) {
+	try {
+		const collection = await dbService.getCollection('toyStore')
+		const { deletedCount } = await collection.deleteOne({ _id: ObjectId.createFromHexString(toyId) })
+		return deletedCount
+	} catch (err) {
+		logger.error(`cannot remove toy ${toyId}`, err)
+		throw err
+	}
+}
+
+async function add(toy) {
+	try {
+		const collection = await dbService.getCollection('toyStore')
+		await collection.insertOne(toy)
+		return toy
+	} catch (err) {
+		logger.error('cannot insert toy', err)
+		throw err
+	}
+}
+
+async function update(toy) {
+	try {
+		const toyToUpdate = toy
+		const collection = await dbService.getCollection('toyStore')
+		await collection.updateOne({ _id: ObjectId.createFromHexString(toy._id) }, { $set: toyToUpdate })
+		return toy
+	} catch (err) {
+		logger.error(`cannot update toy ${toy._id}`, err)
+		throw err
+	}
+}
+
+async function addMsg(Id, msg) {
+	try {
+		msg.id = makeId()
+
+		const collection = await dbService.getCollection('toyStore')
+		await collection.updateOne(
+			{ _id: ObjectId.createFromHexString(Id) },
+			{ $push: { msgs: msg } })
+		return msg
+	} catch (err) {
+		logger.error(`cannot add  msg ${Id}`, err)
+		throw err
+	}
+}
+
+async function removeMsg(Id, msgId) {
+	try {
+		const collection = await dbService.getCollection('toyStore')
+		await collection.updateOne(
+			{ _id: ObjectId.createFromHexString(Id) },
+			{ $pull: { msgs: { id: msgId } } })
+		return msgId
+	} catch (err) {
+		logger.error(`cannot add  msg ${Id}`, err)
+		throw err
+	}
+}
+
+function _buildCriteria(filterBy) {
+	const criteria = {}
+
+	if (filterBy.creatorId) {
+		criteria['creator._id'] = ObjectId.createFromHexString(filterBy.creatorId)
+	}
+
+	if (filterBy.txt) {
+		criteria.name = { $regex: filterBy.txt, $options: 'i' }
+	}
+
+	if (filterBy.inStock !== undefined & filterBy.inStock !== '') {
+		criteria.inStock = true
+	}
+
+	if (filterBy.labels?.length && !filterBy.labels.includes('')) {
+		criteria.labels = { $in: filterBy.labels }
+	}
+
+	if (filterBy.maxPrice) {
+		criteria.maxPrice = { $lte: +filterBy.maxPrice }
+	}
+
+	return criteria
+}
